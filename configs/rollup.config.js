c016b858b3fb89c9e53538a60a342e023c6a1823
olegtempl <linner_952@mail.ru>
2018-06-02 02:48:35 +0200

    update config

---
+++ configs/rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+const path = require('./path.json');
+
+export default {
+  input: path.src.js + path.src.startFileJs,
+  output: {
+    file: path.build.js + path.src.endFileJs,
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+};
