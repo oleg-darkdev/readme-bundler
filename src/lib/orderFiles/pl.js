298b4b450639c703a5be1c3f71e0b6d7d8d2141e
olegtempl <linner_952@mail.ru>
2018-06-08 02:02:43 +0200

    rename directory for order files

---
+++ src/lib/orderFiles/pl.js
@@ -0,0 +1,27 @@
+/*
+  Порядок файлов для сборки файла README
+*/
+ const filesNameListRu = [
+  'logo[RU].md',
+  'introduction[RU].md',
+  'navigation[RU].md',
+  'install[RU].md',
+  'commands[RU].md',
+  'cli[RU].md',
+  'cliGulp[RU].md',
+  'releases[RU].md',
+  'documentation[RU].md',
+  'api[RU].md',
+  'troubleshooting[RU].md',
+  'screenshotes[RU].md',
+  'examples[RU].md',
+  'architecture[RU].md',
+  'stack[RU].md',
+  'license[RU].md',
+  'team_core[RU].md',
+  'contribute[RU].md',
+  'sponsors[RU].md'
+];
+
+// commonjs style for jasmine tests
+module.exports = filesNameListRu;
