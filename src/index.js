bdb0f6d8c919d55234b74c700da891c406758c3a
olegtempl <linner_952@mail.ru>
2018-07-03 18:49:52 +0200

    update library

--- src/index.js
+++ src/index.js
@@ -37,61 +37,76 @@
 // const languageModule = require('../src/lib/lang_modules/en.js');
 
 const cl = require('node-cl-log'),
-      fs = require('fs');
+  fs = require('fs');
 
 const pathDirLanguageModules = './src/lib/lang_modules/';
+const pathDocsDir = './docs/';
 
-let [generationType = '-r', language = 'en'] = [process.argv[2], process.argv[3]];
+let [generationType = '-r', language = 'en'] = [process.argv[2], process.argv[3].slice(1, 3)];
+
+function checkingExistenceDirectory (pathDocsDir) {
+  let dirLanguageModuleFounded = false;
+
+  fs.readdir(pathDocsDir, function (err, items) {
+    items.forEach((el, ind) => {
+      if (el == language.toUpperCase()) dirLanguageModuleFounded = true;
+    });
+  });
+  if (dirLanguageModuleFounded != false) {
+    fs.mkdirSync(`${pathDocsDir + language.toUpperCase()}`);
+    // cl.whi('create dir')
+  };
+};
+
+
+
 // 3 - minimum arguments
 let minArguments = 3;
 if (process.argv.length <= minArguments) {
-    cl.log("Usage: " + __filename + " SOME_PARAM");
-    process.exit(-1);
+  cl.log("Usage: " + __filename + " SOME_PARAM");
+  process.exit(-1);
 } else if (process.argv.length > minArguments) {
-	fs.readdir(pathDirLanguageModules , function (err, items) {
+  fs.readdir(pathDirLanguageModules, function (err, items) {
     let resultFoundLanguageModule = false;
     // cl.log(items);
     // cl.log(language);
-		items.forEach( (el, ind) => {
-      cl.log(el = el.slice(0,2));
-
-      if(el == language) resultFoundLanguageModule = items[ind];
+    items.forEach((el, ind) => {
+      // cl.log(el = el.slice(0, 2));
+      el = el.slice(0, 2)
+      if (el == language) resultFoundLanguageModule = items[ind];
     });
+    if (resultFoundLanguageModule) {
+      // cl.gre('import module !')
+      // import the language module chosen by the user
+      const languageModule = require(`${pathDirLanguageModules + resultFoundLanguageModule}`);
 
+      // cl.log(languageModule);
+      if (generationType == '-d') {
 
-
-    if(resultFoundLanguageModule) {
-      cl.gre('import module !')
-        // import the language module chosen by the user
-        const languageModule = require(`../${pathDirLanguageModules + resultFoundLanguageModule}`);
-
+        checkingExistenceDirectory(pathDocsDir);
+        cl.log('Clone the basic templates on "./docs" directory');
+        languageModule.genDocs();
+        // for(var key in languageModule) {
+        //   cl.log(key + " : " + languageModule[key]);
+        // }
         // cl.log(languageModule);
-          if (generationType == '-d') {
-            languageModule.genDocs();
 
-            // for(var key in languageModule) {
-            //   cl.log(key + " : " + languageModule[key]);
-            // }
-            // cl.log(languageModule);
-
-
-          } else if (generationType == '-r') {
-            cl.log('-r');
-            languageModule.genReadme();
-          }
-          // else if (generationType == '-h') {
-          //   // languageModule.genHtml();
-          // }
-          else {
-            cl.red('The wrong argument, all list of arguments is transferred:')
-          };
+      } else if (generationType == '-r') {
+        cl.whi('')
+        cl.gre('Generate the readme file');
+        cl.whi('')
+        languageModule.genReadme();
+      }
+      // else if (generationType == '-h') {
+      //   // languageModule.genHtml();
+      // }
+      else {
+        cl.red('The wrong argument, all list of arguments is transferred:')
+      };
     } else {
       cl.red(' Warning? Dont find language module!');
       cl.log()
       cl.log('end')
     }
-	}); // end fs.readdir
+  }); // end fs.readdir
 }; // end else if
-
-
-
