8f43301479417477279f8c350fa17deae1cc69d5
olegtempl <linner_952@mail.ru>
2018-09-01 18:07:10 +0200

    update tests, add by,ua,pl

--- test/jasmine/specCli.js
+++ test/jasmine/specCli.js
@@ -1,53 +1,89 @@
-var sys = require('sys')
-var exec = require('child_process').exec;
+const sys = require('sys'),
+  exec = require('child_process').exec,
+  fs = require('extfs');
+const path = require('../../src/lib/path');
+
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
+function clear(pathDirClean) {
+  if (fs.existsSync(pathDirClean)) {
+    fs.readdirSync(pathDirClean).forEach(function (file, index) {
+      let curPath = pathDirClean + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+  };
+};
 
-describe("Cli russian language", function() {
-  it("Start generate the russian templates on docs directory", function() {
+
+describe("Cli russian language", function () {
+  it("Start generate the russian templates on docs directory", function () {
+    clear(path.src.docs.ru);
     exec('npm run doc:ru', puts);
+    // let resultFoundFilesOnDir = res(path.src.docs.ru);
+    // let emtyFile = fs.isEmpty(path.src.docs.ru, function (empty) {
+    //   // empty;
+    //   console.log(empty);
+    //   // resultFoundFilesOnDir  = empty;
+    // });
+
+    // expect(resultFoundFilesOnDir).toBeTruthy();
   });
-  it("Start generate the russian readme file", function() {
-    exec('npm run readme:ru', puts);
-  });
-  it("Start generate the russian html file", function() {
-    exec('npm run html:ru', puts);
-  });
+  // it("Start generate the russian readme file", function() {
+  //   clear(path.build.readme.ru);
+  //   exec('npm run readme:ru', puts);
+  // });
+  // it("Start generate the russian html file", function() {
+  //   clear(path.build.html.ru);
+  //   exec('npm run html:ru', puts);
+  // });
 });
 
-describe("Cli english language", function() {
-  it("Start generate the englishtemplates on docs directory", function() {
-    exec('npm run doc:en', puts);
-  });
-  it("Start generate the english readme file", function() {
-    exec('npm run readme:en', puts);
-  });
-  it("Start generate the english html file", function() {
-    exec('npm run html:en', puts);
-  });
-});
+// describe("Cli english language", function() {
+//   it("Start generate the englishtemplates on docs directory", function() {
+//     clear(path.src.docs.en)
+//     exec('npm run doc:en', puts);
+//   });
+//   it("Start generate the english readme file", function() {
+//     clear(path.build.readme.en);
+//     exec('npm run readme:en', puts);
+//   });
+//   it("Start generate the english html file", function() {
+//     clear(path.build.html.en);
+//     exec('npm run html:en', puts);
+//   });
+// });
 
-describe("Cli Belarusian language", function() {
-  it("Start generate the Belarusian templates on docs directory", function() {
-    exec('npm run doc:by', puts);
-  });
-  it("Start generate the Belarusianreadme file", function() {
-    exec('npm run readme:by', puts);
-  });
-  it("Start generate the Belarusian html file", function() {
-    exec('npm run html:by', puts);
-  });
-});
+// describe("Cli Belarusian language", function() {
+//   it("Start generate the Belarusian templates on docs directory", function() {
+//     clear(path.src.docs.by)
+//     exec('npm run doc:by', puts);
+//   });
+//   it("Start generate the Belarusianreadme file", function() {
+//     clear(path.build.readme.by);
+//     exec('npm run readme:by', puts);
+//   });
+//   it("Start generate the Belarusian html file", function() {
+//     clear(path.build.html.by);
+//     exec('npm run html:by', puts);
+//   });
+// });
 
 
 // describe("Cli Ukraine language", function() {
 //   it("Start generate the Ukraine templates on docs directory", function() {
+// clear(path.src.docs.ua)
 //     exec('npm run doc:ua');
 //   });
 //   it("Start generate the Ukraine readme file", function() {
+// clear(path.build.readme.ua);
 //     exec('npm run readme:ua');
 //   });
 //   it("Start generate the Ukraine html file", function() {
+// clear(path.build.html.ua);
 //     exec('npm run html:ua');
 //   });
 // });
@@ -55,17 +91,18 @@ describe("Cli Belarusian language", function() {
 
 // describe("Cli Polyakcha language", function() {
 //   it("Start generate the Polyakcha templates on docs directory", function() {
+// clear(path.src.docs.pl)
+// clear(path.build.readme.pl);
 //     exec('npm run doc:pl');
 //   });
 //   it("Start generate the Polyakcha readme file", function() {
 //     exec('npm run readme:pl');
 //   });
 //   it("Start generate the Polyakcha html file", function() {
+// clear(path.build.html.pl);
 //     exec('npm run html:pl');
 //   });
 // });
-
-
 
 
 
